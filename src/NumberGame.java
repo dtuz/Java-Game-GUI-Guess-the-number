f03fc8e4e3ece39f1c42a98d6c99f30f71d903ed
Daryna <darynatuzova@gmail.com>
2024-06-09 21:10:11 +0200

    code refactoring

--- src/NumberGame.java
+++ src/NumberGame.java
@@ -3,108 +3,105 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class NumberGame {
+    private static final String GUESS_TEXT = "Your number is: ";
+
     private double guessedNumber;
-    private double lowLimit = 1;
-    private double highLimit = 101;
+    private double lowLimit;
+    private double highLimit;
     private int counter = 0;
 
     private JPanel NumberGame;
-    private JLabel GameLabel;
     private JTextField textField;
     private JButton buttonSmaller;
     private JButton buttonGreater;
     private JTextField attempts;
     private JButton buttonStart;
     private JButton buttonGuessedNumber;
+    private JLabel GameLabel;
+    private final JFrame frame;
 
-    public NumberGame(){
-        String text = textField.getText();
+    public NumberGame() {
         buttonSmaller.setVisible(false);
         buttonGuessedNumber.setVisible(false);
         buttonGreater.setVisible(false);
         textField.setFocusable(false);
         attempts.setFocusable(false);
-        JFrame frame = new JFrame("Input Dialog Example");
+
+        frame = new JFrame("Input Dialog Example");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(300, 200);
 
         buttonStart.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
-                if(highLimit != 100 && lowLimit != 0){
-                    buttonSmaller.setVisible(false);
-                    buttonGreater.setVisible(false);
-                    textField.setText(text);
-                    counter = 0;
-                }
-
-                guessedNumber = 50;
-                buttonSmaller.setVisible(true);
-                buttonGreater.setVisible(true);
-                buttonStart.setVisible(false);
-                buttonGuessedNumber.setVisible(true);
-                textField.setText(text + String.valueOf((int) guessedNumber) + "?");
-                counter++;
-                attempts.setText(String.valueOf(counter));
+                setGameState(true);
             }
         });
         buttonSmaller.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String message = "<html>The number you wished is: " + (int) guessedNumber + "<br>I guessed it in " + counter + " tries</html>";
-                if
-                (lowLimit == guessedNumber - 1){
-                    JOptionPane.showMessageDialog(frame, message, "Victory", JOptionPane.INFORMATION_MESSAGE);
-                    buttonStart.setVisible(true);
-                    buttonGreater.setEnabled(false);
+                if (lowLimit == guessedNumber - 1) {
+                    guessedNumber--;
+                    setGameState(false);
                     return;
                 }
 
                 highLimit = (int) guessedNumber;
-                guessedNumber = Math.floor((highLimit + lowLimit) / 2);
 
-                textField.setText(text + String.valueOf((int) guessedNumber) + "?");
-                counter++;
-                attempts.setText(String.valueOf(counter));
-
+                updateAttempts();
             }
         });
         buttonGreater.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String message = "<html>The number you wished is: " + (int) guessedNumber + "<br>I guessed it in " + counter + " tries</html>";
-                if(highLimit == guessedNumber + 1){
-                    JOptionPane.showMessageDialog(frame, message, "Victory", JOptionPane.INFORMATION_MESSAGE);
-                    buttonStart.setVisible(true);
-                    buttonSmaller.setVisible(false);
-                    buttonGreater.setVisible(false);
-                    buttonGuessedNumber.setVisible(false);
+
+                if (highLimit == guessedNumber + 1) {
+                    guessedNumber++;
+                    setGameState(false);
                     return;
                 }
 
                 lowLimit = (int) guessedNumber;
-                guessedNumber = Math.floor((highLimit + guessedNumber) / 2);
 
-                textField.setText(text + String.valueOf((int) guessedNumber) + "?");
-                counter++;
-                attempts.setText(String.valueOf(counter));
-
+                updateAttempts();
             }
         });
         buttonGuessedNumber.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String message = "<html>The number you wished is: " + (int) guessedNumber + "<br>I guessed it in " + counter + " tries</html>";
-
-                JOptionPane.showMessageDialog(frame, message, "Victory", JOptionPane.INFORMATION_MESSAGE);
-
-                buttonGuessedNumber.setVisible(false);
-                buttonGreater.setVisible(false);
-                buttonSmaller.setVisible(false);
-                buttonStart.setVisible(true);
+                setGameState(false);
             }
         });
+    }
+
+    private void updateAttempts() {
+        guessedNumber = Math.floor((highLimit + lowLimit) / 2);
+        textField.setText(GUESS_TEXT + ((int) guessedNumber) + "?");
+        counter++;
+        attempts.setText(String.valueOf(counter));
+    }
+
+    private void setGameState(boolean state) {
+        buttonSmaller.setVisible(state);
+        buttonGreater.setVisible(state);
+        buttonGuessedNumber.setVisible(state);
+        buttonStart.setVisible(!state);
+
+        if (state) {
+            counter = 1;
+            guessedNumber = 50;
+            lowLimit = 1;
+            highLimit = 100;
+
+            textField.setText(GUESS_TEXT + ((int) guessedNumber) + "?");
+            attempts.setText(String.valueOf(counter));
+            return;
+        }
+
+        textField.setText("");
+        attempts.setText("");
+        String message = "<html>The number you wished is: " + (int) guessedNumber + "<br>I guessed it in " + counter + " tries</html>";
+        JOptionPane.showMessageDialog(frame, message, "Victory", JOptionPane.INFORMATION_MESSAGE);
     }
 
     public static void main(String[] args) {
@@ -113,9 +110,6 @@ public class NumberGame {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
-
     }
-
-
 
 }
